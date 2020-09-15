cab00e794ba9a6105e07c994724d59c14d589621
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-15 16:17:57 +0600

    views and urls updated

--- snippets/views.py
+++ snippets/views.py
@@ -1,8 +1,9 @@
+from django.contirb.auth.models import User
 from rest_framework import mixins
 from rest_framework import generics
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -18,5 +19,9 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	Retrieve, update or delete a snippet instance
 	"""
 	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
+	serializer_class = UserSerializer
 		
+
+class UserDetail(generics.RetrieveAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
